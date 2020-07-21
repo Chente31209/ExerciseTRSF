d864b7c2c5edeb3ec800c8981a06d1b6b0a83e0b
eduardo martinez <chente31209@hotmail.com>
2020-07-21 16:59:23 -0500

    se relisaron las coreciones corepondites a los errores de compilacion -<solo fallo el comado de migraion no lo asepto >-

--- mvc_Migration/Models/SeedData.cs
+++ mvc_Migration/Models/SeedData.cs
@@ -17,11 +17,11 @@ namespace mvc_Migration.Models
             }
             if (!context.Emails.Any()) {
                 context.Emails.AddRange(
-                    new Email{ E_mailDirtetion= "kjbfdgz@vhneouh.com" ,Pasword="ljagn",Id=1,Id_user=1,Id_permissions=100},
-                    new Email{E_mailDirtetion= "jkgabpo@vhneouh.com" ,Pasword="ojr ngea",Id=10,Id_user=2,Id_permissions=110},
-                    new Email{E_mailDirtetion= "jrrgjmpi@vhneouh.com" ,Pasword="lsotkrhnow",Id=16,Id_user=3,Id_permissions=111},
-                    new Email{E_mailDirtetion= "jrh79@vhneouh.com" ,Pasword="lsotk6u0rhnow",Id=18,Id_user=4,Id_permissions=001},
-                    new Email{E_mailDirtetion= "pretis0j9@vhneouh.com" ,Pasword="wjsrtyu.ry",Id=54,Id_user=5,Id_permissions=101}
+                    new Email{ E_mailDirtetion= "kjbfdgz@vhneouh.com" ,Pasword="ljagn",Id_user=1,Id_permissions=100},
+                    new Email{E_mailDirtetion= "jkgabpo@vhneouh.com" ,Pasword="ojr ngea",Id_user=2,Id_permissions=110},
+                    new Email{E_mailDirtetion= "jrrgjmpi@vhneouh.com" ,Pasword="lsotkrhnow",Id_user=3,Id_permissions=111},
+                    new Email{E_mailDirtetion= "jrh79@vhneouh.com" ,Pasword="lsotk6u0rhnow",Id_user=4,Id_permissions=001},
+                    new Email{E_mailDirtetion= "pretis0j9@vhneouh.com" ,Pasword="wjsrtyu.ry",Id_user=5,Id_permissions=101}
                 );
                 context.SaveChanges();
             }
